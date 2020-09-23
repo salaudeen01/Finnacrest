f9e8fb28a60d457c431ab23c0f1bc286636c27b3
simplyremisimple <abaremi8@gmail.com>
2020-09-23 18:20:25 +0100

    forgot password

--- src/app/views/sessions/ForgotPassword.jsx
+++ src/app/views/sessions/ForgotPassword.jsx
@@ -1,134 +1,209 @@
-import React, { Component } from "react";
-import { Card, Grid, Button, CircularProgress, Typography } from "@material-ui/core";
-import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
-import swal from 'sweetalert'
-import { userActions } from "../../redux/actions/user.actions";
-import { checkToken } from '../../config/config'
+import React, { Component } from 'react';
+import {
+	Card,
+	Grid,
+	Button,
+	CircularProgress,
+	Typography,
+} from '@material-ui/core';
+import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import swal from 'sweetalert';
+import { userActions } from '../../redux/actions/user.actions';
+import { checkToken } from '../../config/config';
 
-const styles = theme => ({
-  wrapper: {
-    position: "relative"
-  },
+const styles = (theme) => ({
+	wrapper: {
+		position: 'relative',
+	},
 
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12
-  }
+	buttonProgress: {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		marginTop: -12,
+		marginLeft: -12,
+	},
 });
 
 class ForgotPassword extends Component {
-  state = {
-    data: {email:""}
-  };
-  handleChange = event => {
-    event.persist();
-    this.setState({data:
-      {[event.target.name]: event.target.value}
-    });
-  };
-  handleFormSubmit = () => {
-    const {data} = this.state
-    if (data.email) {
-      this.props.recoverpass(data);
-  }else{
-    swal(data.email);
-}
-    
-  };
-  render() {
-    let { data } = this.state;
-    let { classes } = this.props;
+	state = {
+		data: { email: '' },
+	};
+	handleChange = (event) => {
+		event.persist();
+		this.setState({ data: { [event.target.name]: event.target.value } });
+	};
+	handleFormSubmit = () => {
+		const { data } = this.state;
+		if (data.email) {
+			this.props.recoverpass(data);
+		} else {
+			swal(data.email);
+		}
+	};
+	render() {
+		let { data } = this.state;
+		let { classes } = this.props;
 
-    return (
-      <div className="signup flex justify-center w-full h-full-screen" style={{
-        backgroundImage: `url(${"/assets/images/bg.png"})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-        backgroundAttachment: "fixed",
-        }}>
-        <div className="block mt-10">
-          <div className="p-8">
-            <Grid container className="p-2 " justify="center" alignItems="center">
-              <Grid lg={6} md={6} sm={6} xs={6}>
-                <img src="/assets/images/Group24.png"/>
-              </Grid>
-            </Grid>
-          </div>
-          <Card className="signup-card" >
-            <Grid container className="p-12 bg-light-gray" direction="column" justify="center" alignItems="center">
-              <Grid lg={12} md={12} sm={12} xs={12}>
-                <Typography variant="h6" className="text-center text-gray mb-4">Recover Password</Typography>
-              </Grid>
-              <ValidatorForm ref="form" onSubmit={this.handleFormSubmit}>
-                <Grid item lg={12} md={12} sm={12} xs={12}>
-                  {/* <div className="p-9 h-full position-relative"> */}
-                      <TextValidator
-                        className="mb-6 w-full"
-                        variant="outlined"
-                        label="Email"
-                        onChange={this.handleChange}
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        validators={["required", "isEmail"]}
-                        errorMessages={[
-                          "this field is required",
-                          "email is not valid"
-                        ]}/>
-                  {/* </div> */}
-                  </Grid>
-                  <Grid item lg={12} md={12} sm={12} xs={12} className="mb-4">
-                    <Button
-                      variant="contained"
-                      color="warning"
-                      disabled={this.props.loggingIn}
-                      type="submit"
-                      className="capitalize font-medium"
-                      style={{background:'#04956b', color:"#fff", width:"100%"}}
-                    >
-                      Reset
-                    </Button>
-                    {this.props.loggingIn && (
-                      <CircularProgress
-                        size={24}
-                        className={classes.buttonProgress}
-                      />)}
-                  </Grid>
-                  
-                  <Grid item lg={12} md={12} sm={12} xs={12} className="mb-1" style={{textAlign:"center"}}>
-                      <span className="mr-2 ml-5">Already have an account?</span>
-                      <Button
-                        className="capitalize font-medium"
-                        onClick={() =>
-                        this.props.history.push("/signin")
-                        }>
-                        Sign in
-                      </Button>
-                  </Grid>
-              </ValidatorForm>
-            </Grid>
-          </Card>
-      </div>
-      </div>
-    );
-  }
+		return (
+			<div
+				className='signup flex justify-center w-full h-full-screen'
+				style={{
+					backgroundColor: '#0a131b',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center center',
+					backgroundSize: 'cover',
+					backgroundAttachment: 'fixed',
+				}}>
+				<div className='p-4 flex w-full h-full-screen'>
+					<Grid
+						container
+						className='p-2'
+						spacing={3}
+						justify='center'
+						alignItems='center'>
+						<Grid lg={11} md={11} sm={11} xs={11}>
+							<Card className='' style={{ background: '#224459' }}>
+								<Grid container className='p-2' spacing={3}>
+									<Grid lg={4} md={4} sm={6} xs={6}>
+										<Card className='signup-card '>
+											<Grid
+												container
+												className='p-2 bg-light-gray'
+												justify='center'
+												alignItems='center'>
+												<div className='p-4 mt-10'>
+													<Grid
+														container
+														className='p-2 '
+														justify='center'
+														alignItems='center'>
+														<Grid lg={6} md={6} sm={6} xs={6}>
+															<img src='/assets/images/sesis.jpg' />
+														</Grid>
+													</Grid>
+												</div>
+												<Grid lg={12} md={12} sm={12} xs={12}>
+													<Typography
+														variant='h4'
+														className='text-center text-gray'>
+														Recover Password
+													</Typography>
+												</Grid>
+											</Grid>
+											<Grid container className='bg-light-gray'>
+												<ValidatorForm
+													ref='form'
+													onSubmit={this.handleFormSubmit}>
+													<Grid item lg={12} md={12} sm={12} xs={12}>
+														<div className='p-9 h-full position-relative'>
+															<TextValidator
+																className='mb-6 w-full'
+																variant='outlined'
+																label='Email'
+																onChange={this.handleChange}
+																type='email'
+																name='email'
+																value={data.email}
+																validators={['required', 'isEmail']}
+																errorMessages={[
+																	'this field is required',
+																	'email is not valid',
+																]}
+															/>
+
+															<Grid
+																item
+																lg={12}
+																md={12}
+																sm={12}
+																xs={12}
+																className='mb-4'>
+																<Button
+																	variant='contained'
+																	color='warning'
+																	disabled={this.props.loggingIn}
+																	type='submit'
+																	className='capitalize font-medium'
+																	style={{
+																		background: '#224459',
+																		color: '#fff',
+																		width: '84%',
+																		padding: 10,
+																		marginLeft: 36,
+																		marginRight: 36,
+																	}}>
+																	Reset
+																</Button>
+																{this.props.loggingIn && (
+																	<CircularProgress
+																		size={24}
+																		className={classes.buttonProgress}
+																	/>
+																)}
+															</Grid>
+														</div>
+														<Grid
+															item
+															lg={12}
+															md={12}
+															sm={12}
+															xs={12}
+															className='mb-1'
+															style={{ textAlign: 'center' }}>
+															<span className='mr-2 ml-5'>
+																Already have an account?
+															</span>
+															<Button
+																className='capitalize font-medium'
+																onClick={() =>
+																	this.props.history.push('/signin')
+																}>
+																Sign In
+															</Button>
+														</Grid>
+													</Grid>
+												</ValidatorForm>
+											</Grid>
+										</Card>
+									</Grid>
+
+									<Grid lg={8} md={8} sm={6} xs={6}>
+										<Grid
+											container
+											className='p-2 mt-8'
+											justify='center'
+											alignItems='center'>
+											<Typography
+												variant='h4'
+												className='text-center text-white mt-25'
+												style={{ fontWeight: 'bold', width: '10' }}>
+												Welcome to <br />
+												SESIS <br /> Co-OPERATIVE SOCIETY
+											</Typography>
+										</Grid>
+									</Grid>
+								</Grid>
+							</Card>
+						</Grid>
+					</Grid>
+				</div>
+			</div>
+		);
+	}
 }
 const actionCreators = {
-  recoverpass: userActions.recoverpass,
+	recoverpass: userActions.recoverpass,
 };
 
-const mapStateToProps = state => {
-  const { loggingIn } = state.authentication;
-  return { loggingIn };
+const mapStateToProps = (state) => {
+	const { loggingIn } = state.authentication;
+	return { loggingIn };
 };
 export default withStyles(styles, { withTheme: true })(
-  withRouter(connect(mapStateToProps,  actionCreators)(ForgotPassword))
+	withRouter(connect(mapStateToProps, actionCreators)(ForgotPassword))
 );
--- src/app/views/sessions/SignUp.jsx
+++ src/app/views/sessions/SignUp.jsx
@@ -88,170 +88,210 @@ class SignUp extends Component {
       <div
         className='signup flex justify-center w-full h-full-screen'
         style={{
-          backgroundImage: `url(${"/assets/images/bg.png"})`,
+          backgroundColor: "#0a131b",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center center",
           backgroundSize: "cover",
           backgroundAttachment: "fixed",
         }}
       >
-        <div className=' block mt-10'>
-          <div className='p-8'>
-            <Grid
-              container
-              className='p-2 '
-              justify='center'
-              alignItems='center'
-            >
-              <Grid lg={6} md={6} sm={6} xs={6}>
-                <img src='/assets/images/Group 24.png' />
-              </Grid>
+        <div className='p-4 flex w-full h-full-screen'>
+          <Grid
+            container
+            className='p-2'
+            spacing={3}
+            justify='center'
+            alignItems='center'
+          >
+            <Grid lg={11} md={11} sm={11} xs={11}>
+              <Card className='' style={{ background: "#224459" }}>
+                <Grid container className='p-2' spacing={3}>
+                  <Grid lg={4} md={4} sm={6} xs={6}>
+                    <Card className='signup-card '>
+                      <Grid
+                        container
+                        className='p-2 bg-light-gray'
+                        justify='center'
+                        alignItems='center'
+                      >
+                        <div className='p-4 mt-6'>
+                          <Grid
+                            container
+                            className='p-2 '
+                            justify='center'
+                            alignItems='center'
+                          >
+                            <Grid lg={6} md={6} sm={6} xs={6}>
+                              <img src='/assets/images/sesis.jpg' />
+                            </Grid>
+                          </Grid>
+                        </div>
+                        <Grid lg={12} md={12} sm={12} xs={12}>
+                          <Typography
+                            variant='h4'
+                            className='text-center text-gray'
+                          >
+                            Sign Up
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Grid container className='bg-light-gray'>
+                        <ValidatorForm
+                          ref='form'
+                          onSubmit={this.handleFormSubmit}
+                        >
+                          <Grid item lg={12} md={12} sm={12} xs={12}>
+                            <div className='p-9 h-full position-relative'>
+                              <TextValidator
+                                className='mb-6 w-full'
+                                variant='outlined'
+                                label='First Name'
+                                onChange={this.handleChange}
+                                type='text'
+                                name='first_name'
+                                value={data.first_name}
+                                validators={["required"]}
+                                errorMessages={["this field is required"]}
+                              />
+                              <TextValidator
+                                className='mb-6 w-full'
+                                variant='outlined'
+                                label='Last Name'
+                                onChange={this.handleChange}
+                                type='text'
+                                name='last_name'
+                                value={data.last_name}
+                                validators={["required"]}
+                                errorMessages={["this field is required"]}
+                              />
+                              <TextValidator
+                                className='mb-4 w-full'
+                                variant='outlined'
+                                label='Phone Number'
+                                onChange={this.handleChange}
+                                type='type'
+                                name='phone_number'
+                                value={data.phone_no}
+                                validators={["required"]}
+                                errorMessages={["this field is required"]}
+                              />
+                              <TextValidator
+                                className='mb-6 w-full'
+                                variant='outlined'
+                                label='Email'
+                                onChange={this.handleChange}
+                                type='email'
+                                name='email'
+                                value={data.email}
+                                validators={["required", "isEmail"]}
+                                errorMessages={[
+                                  "this field is required",
+                                  "Email is not valid",
+                                ]}
+                              />
+                              <TextValidator
+                                className='mb-4 w-full'
+                                variant='outlined'
+                                label='Password'
+                                onChange={this.handleChange}
+                                type='password'
+                                name='password'
+                                value={data.password}
+                                validators={["required"]}
+                                errorMessages={["this field is required"]}
+                              />
+                              <TextValidator
+                                className='mb-4 w-full'
+                                variant='outlined'
+                                label='Confirm Password'
+                                onChange={this.handleChange}
+                                type='password'
+                                name='confirm_password'
+                                value={data.password}
+                                validators={["required"]}
+                                errorMessages={["this field is required"]}
+                              />
+                            </div>
+                            <Grid
+                              item
+                              lg={12}
+                              md={12}
+                              sm={12}
+                              xs={12}
+                              className='mb-4'
+                            >
+                              <Button
+                                variant='contained'
+                                color='warning'
+                                disabled={this.props.loggingIn}
+                                type='submit'
+                                className='capitalize font-medium'
+                                style={{
+                                  background: "#04956b",
+                                  color: "#fff",
+                                  width: "84%",
+                                  padding: 10,
+                                  marginLeft: 36,
+                                  marginRight: 36,
+                                }}
+                              >
+                                Sign Up
+                              </Button>
+                              {this.props.registering && (
+                                <CircularProgress
+                                  size={24}
+                                  className={classes.buttonProgress}
+                                />
+                              )}
+                            </Grid>
+
+                            <Grid
+                              item
+                              lg={12}
+                              md={12}
+                              sm={12}
+                              xs={12}
+                              className='mb-1'
+                              style={{ textAlign: "center" }}
+                            >
+                              <span className='mr-2 ml-5'>
+                                Already a member?
+                              </span>
+                              <Button
+                                className='capitalize font-medium'
+                                onClick={() =>
+                                  this.props.history.push("/signin")
+                                }
+                              >
+                                Sign In
+                              </Button>
+                            </Grid>
+                          </Grid>
+                        </ValidatorForm>
+                      </Grid>
+                    </Card>
+                  </Grid>
+                  <Grid lg={8} md={8} sm={6} xs={6}>
+                    <Grid
+                      container
+                      className='p-2 mt-8'
+                      justify='center'
+                      alignItems='center'
+                    >
+                      <Typography
+                        variant='h4'
+                        className='text-center text-white mt-25'
+                        style={{ fontWeight: "bold", width: "10" }}
+                      >
+                        Welcome to <br />
+                        SESIS <br /> Co-OPERATIVE SOCIETY
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Card>
             </Grid>
-          </div>
-          <div className='p-4'>
-            <Card className='signup-card '>
-              <Grid
-                container
-                className='p-2 mb-5'
-                justify='center'
-                alignItems='center'
-              >
-                <Grid lg={12} md={12} sm={12} xs={12}>
-                  <Typography variant='h4' className='text-center text-gray'>
-                    Sign Up
-                  </Typography>
-                </Grid>
-              </Grid>
-              <ValidatorForm ref='form' onSubmit={this.handleFormSubmit}>
-                <Grid container className='px-5' spacing={2}>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    {/* <div className="p-9 h-full"> */}
-                    <TextValidator
-                      className='mb-6 w-full'
-                      variant='outlined'
-                      label='First Name'
-                      onChange={this.handleChange}
-                      type='text'
-                      name='first_name'
-                      value={data.first_name}
-                      validators={["required"]}
-                      errorMessages={["this field is required"]}
-                    />
-                  </Grid>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    <TextValidator
-                      className='mb-6 w-full'
-                      variant='outlined'
-                      label='Last Name'
-                      onChange={this.handleChange}
-                      type='text'
-                      name='last_name'
-                      value={data.last_name}
-                      validators={["required"]}
-                      errorMessages={["this field is required"]}
-                    />
-                  </Grid>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    <TextValidator
-                      className='mb-4 w-full'
-                      label='Phone Number'
-                      variant='outlined'
-                      onChange={this.handleChange}
-                      name='phone_no'
-                      type='text'
-                      value={data.phone_no}
-                      validators={["required"]}
-                      errorMessages={["this field is required"]}
-                    />
-                  </Grid>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    <TextValidator
-                      className='mb-6 w-full'
-                      variant='outlined'
-                      label='Email'
-                      onChange={this.handleChange}
-                      type='email'
-                      name='email'
-                      value={data.email}
-                      validators={["required", "isEmail"]}
-                      errorMessages={[
-                        "this field is required",
-                        "Email is not valid",
-                      ]}
-                    />
-                  </Grid>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    <TextValidator
-                      className='mb-4 w-full'
-                      label='Password'
-                      variant='outlined'
-                      onChange={this.handleChange}
-                      name='password'
-                      type='password'
-                      value={data.password}
-                      validators={["required"]}
-                      errorMessages={["this field is required"]}
-                    />
-                  </Grid>
-                  <Grid item lg={6} md={6} sm={12} xs={12}>
-                    <TextValidator
-                      className='mb-4 w-full'
-                      label='Confirm Password'
-                      variant='outlined'
-                      onChange={this.handleChange}
-                      name='confirm_password'
-                      type='password'
-                      value={confirm_password}
-                      validators={["required"]}
-                      errorMessages={["this field is required"]}
-                    />
-                  </Grid>
-                  <Grid item lg={12} md={12} sm={12} xs={12} className='mb-4'>
-                    <Button
-                      variant='contained'
-                      color='warning'
-                      disabled={this.props.loggingIn}
-                      type='submit'
-                      className='capitalize font-medium'
-                      style={{
-                        background: "#04956b",
-                        color: "#fff",
-                        width: "84%",
-                        padding: 10,
-                        marginLeft: 36,
-                        marginRight: 36,
-                      }}
-                    >
-                      Sign up
-                    </Button>
-                    {this.props.registering && (
-                      <CircularProgress
-                        size={24}
-                        className={classes.buttonProgress}
-                      />
-                    )}
-                  </Grid>
-                  <Grid
-                    item
-                    lg={12}
-                    md={12}
-                    sm={12}
-                    xs={12}
-                    className='mb-1'
-                    style={{ textAlign: "center" }}
-                  >
-                    <span className='mr-2 ml-5'>Already a member?</span>
-                    <Button
-                      className='capitalize font-medium'
-                      onClick={() => this.props.history.push("/signin")}
-                    >
-                      Sign in
-                    </Button>
-                  </Grid>
-                </Grid>
-              </ValidatorForm>
-            </Card>
-          </div>
+          </Grid>
         </div>
       </div>
     );
