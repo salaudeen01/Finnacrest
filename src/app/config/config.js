efc697b392d2ab52dab6aa67fbdd72d58e8c5821
Salaudeen01 <salaudeen.adedeji4real@yahoo.com>
2021-06-15 17:11:42 +0100

    test_app

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,7 +1,7 @@
 import history from '../../history';
 const serverVars = {
-  // baseUrl: "http://142.93.152.229/sesis/api/",
-  baseUrl: "https://api.sesiscoop.com/api/",
+  baseUrl: "http://142.93.152.229/sesis/api/",
+  // baseUrl: "https://api.sesiscoop.com/api/",
   authUrl: "auth/login",
   regUrl: "auth/signup",
   resetPass: "profilesChangePassword?token=",
@@ -168,8 +168,8 @@ export const numberFormat = (value) =>
   }).format(value);
 
 export const payID = () => {
-  // return "pk_test_c236c6a4facaed7a4cb7968769410ca980c10fdf";
-  return "pk_live_9f94d021a79f4c888fe318dd975c2f074ca5606a";
+  return "pk_test_c236c6a4facaed7a4cb7968769410ca980c10fdf";
+  // return "pk_live_9f94d021a79f4c888fe318dd975c2f074ca5606a";
 
 };
 
